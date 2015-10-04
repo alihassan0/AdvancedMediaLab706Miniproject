32e72406bd95290bbf7fdc6b792b8742be6bffe3
alithephantom <alithephantom@gmail.com>
2015-10-04 13:22:53 +0200

    added data in table fill

--- create_table.php
+++ create_table.php
@@ -9,13 +9,13 @@ else echo "error in db";
 mysqli_select_db($con, "eBay");
 mysqli_query($con, "CREATE TABLE user(eMail varchar(50) NOT NULL,firstName varchar(20) NOT NULL,
 					lastName varchar(20) NOT NULL,password varchar(50) NOT NULL,avatar varchar(100),
-					PRIMARY KEY(eMail))");
+					PRIMARY KEY(eMail));");
 mysqli_query($con, "CREATE TABLE product(id int NOT NULL AUTO_INCREMENT ,name varchar(20),
-					stock int NOT NULL, description varchar(100) , avatar varchar(100), PRIMARY KEY(id));");
+					stock int NOT NULL, description varchar(100) , thumbnail varchar(100), PRIMARY KEY(id));");
 
 mysqli_query($con, "CREATE TABLE transaction(id int NOT NULL , eMail varchar(20) NOT NULL ,sell int,
 					FOREIGN KEY (eMail) REFERENCES user(eMail),FOREIGN KEY (id) REFERENCES product(id),
-					PRIMARY KEY(id,eMail))");
+					PRIMARY KEY(id,eMail));");
 
 ?>
 <?php 
--- table_fill.php
+++ table_fill.php
@@ -4,5 +4,11 @@ ini_set( 'display_errors', 'On' );
 
 $con = mysqli_connect( "localhost", "root", "", "eBay" ) or die( mysql_error() );
 mysqli_query( $con, "insert into user(eMail,firstname,lastname,password) values('omar.ihab.12@gmail.com','omar','omar','omar')" );
-mysqli_query($con , "insert into product(name,eMail,stock) values('ay7aga','omar',5)");
+$text = file_get_contents("public_html/js/data.json");
+$json = json_decode($text, true);//parse Json data
+
+foreach ($json['data'] as $key => $value)
+{
+	mysqli_query($con , "insert into product(name,thumbnail,stock,description) values('$value[name]','$value[path]',5 , 'fresh')");
+}
 ?>
