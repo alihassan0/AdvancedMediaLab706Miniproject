776dea61f1b22c8a5a6ef69040708ecbc84b88b4
OmarIhab12 <omar.ihab.12@gmail.com>
2015-10-04 17:02:32 +0200

    backend update

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -12,8 +12,48 @@ $(document).ready(function() {
 	  		$(".error").empty();
             $(".error").append(alertMsg);
 	    }
-	    alert("asd");
+	    alert("nmr");
 	  });
   });
 });
 var alertMsg = '<div class="alert alert-danger" role="alert"> either your email or password are incorrect .. please try again</div>'
+
+
+$(document).ready(function() {
+  $(".signup").click(function () {
+  	  var ajaxurl = 'ajax.php',
+	  data =  {'action': 'signup','eMail': $("#eMail").val(), 'password': $("#password").val() , 'firstname': $("#fName").val() , 'lastname': $("#lName").val()};
+	  $.post(ajaxurl, data, function (response) {
+	  	if(response == 0)
+	  	{
+	  		window.location = "home.php";
+	  	}	
+	    else
+	    {
+	  		$(".error").empty();
+            $(".error").append(alertMsg);
+	    }
+	    alert("asd");
+	  });
+  });
+}); 
+
+$(document).ready(function() {
+  $(".editprofile").click(function () {
+  	  var ajaxurl = 'ajax.php',
+	  data =  {'action': 'editprofilr','eMail': $("#eMail").val(), 'password': $("#password").val() , 'firstname': $("#fName").val() , 'lastname': $("#lName").val() 
+	                                  ,'avatar': $("#avatar").val() , 'newpassword' : ("#newpassword")};
+	  $.post(ajaxurl, data, function (response) {
+	  	if(response == 0)
+	  	{
+	  		window.location = "home.php";
+	  	}	
+	    else
+	    {
+	  		$(".error").empty();
+            $(".error").append(alertMsg);
+	    }
+	    alert("asd");
+	  });
+  });
+}); 
